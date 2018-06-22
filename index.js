e1f6f4cc2b762296911f83bf4db12e176f426413
Crispin Müller <craisp@posteo.de>
2018-06-22 12:18:34 +0200

    Initialize repository

---
+++ index.js
@@ -0,0 +1,16 @@
+// Reverses a string.
+function reverse(string) {
+  return Array.from(string).reverse().join("");
+}
+
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
+
+  // Returns true for a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    let processedContent = this.content.toLowerCase();
+    return processedContent === reverse(processedContent);
+  }
+
+}
